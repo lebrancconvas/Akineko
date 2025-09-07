665374a7e15478958987e197d5a86e5a3aed68a8
lebrancconvas <lebrancconvas@gmail.com>
2025-09-07 15:50:39 +0700

    ✨ feat(frontend): update user register form ui.

---
+++ frontend/src/pages/register.tsx
@@ -0,0 +1,18 @@
+function Register() {
+  return (
+    <div>
+      <form action="">
+        <label>Username</label>
+        <input type="text" name="username" id="username" />
+
+        <label>Password</label>
+        <input type="password" name="password" id="password" />
+
+        <label>Display Name</label>
+        <input type="text" name="display" id="display" />
+      </form>
+    </div>
+  )
+};
+
+export default Register;
