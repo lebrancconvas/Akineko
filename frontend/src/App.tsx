6b508edb823e3792c02542ba608e6896a98d48e5
lebrancconvas <lebrancconvas@gmail.com>
2025-09-07 15:50:21 +0700

    ✨ feat(frontend): update app.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <>
       <header>
         <h1>Akineko</h1>
       </header>
+      <footer>
+        <Footer />
+      </footer>
     </>
   )
 };
