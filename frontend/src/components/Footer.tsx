cb6a9e69a9dc1bafa3bb672b06f15fd37b4a6042
lebrancconvas <lebrancconvas@gmail.com>
2025-09-07 15:50:08 +0700

    💄 style(frontend): update footer and its style.

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import FooterStyle from "../styles/Footer.module.css";
+
+function Footer() {
+  return (
+    <div className={FooterStyle.footer}>
+      <p>Made with Love by <a href="https://github.com/lebrancconvas" target="_blank">Poom Yimyuean (lebrancconvas)</a></p>
+    </div>
+  )
+};
+
+export default Footer;
